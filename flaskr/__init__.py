d593b1b309f6b58cbec266620859139ff1b0d071
Christopher Won <christopher.won@queensu.ca>
2020-04-26 20:10:31 -0400

    fixed syntax

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -25,5 +25,9 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World'
+    
+    from . import game
+    app.register_blueprint(game.bp)
+    app.add_url_rule('/',endpoint='game')
 
     return app
--- flaskr/model.py
+++ flaskr/model.py
@@ -1,20 +1,31 @@
 max_chain = 1
 
-def create_state(int r, int c):
-    global state = [[0] * r for i in range(c)]
+def create_state(r, c):
+    try:
+        global state 
+        state = [[0] * r for i in range(c)]
+        return True
+    except:
+        return False
 
 
-def update_state(int r, int c, int chain=0):
-    if state[r][c] = 0:
-        global state[r][c] = 1
-    else:
-        global state[r][c] = 0
+def update_state(r, c, chain=0):
+    global state
+    try:
+        if state[r][c] == 0:
+            state[r][c] = 1
+        else:
+            state[r][c] = 0
 
-    if chain != max_chain:
-        update_state(r+1, c, 1)
-        update_state(r-1, c, 1)
-        update_state(r, c+1, 1)
-        update_state(r, c-1, 1)
+        if chain != max_chain:
+            update_state(r+1, c, 1)
+            update_state(r-1, c, 1)
+            update_state(r, c+1, 1)
+            update_state(r, c-1, 1)
+        
+        return True
+    except:
+        return False
 
 def get_state():
     return state
