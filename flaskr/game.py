995aef872c7b0b02ddd89667613fd7a60e3dbc0d
Christopher Won <christopher.won@queensu.ca>
2020-04-30 17:36:47 -0400

    added html and css files

--- flaskr/game.py
+++ flaskr/game.py
@@ -2,26 +2,36 @@ from flask import (
         Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
         )
 
-import flaskr.model
+from flaskr.model import *
 
 bp = Blueprint('game', __name__, url_prefix='/game')
 
 @bp.route('/running', methods=['GET'])
 def running():
-    state = model.get_state()
-    return render_template('game/running.html',state)
+    state = get_state()
+    if state == False:
+        return redirect(url_for('.start'))
+    return render_template('game/running.html',state=state, rows=len(state),columns=len(state[0]))
 
-@bp.route('/running_state/<int:row>_<int:column>', methods=['GET', 'POST'])
+@bp.route('/state_change')
+def state_change():
+    row = int(request.args.get('row'))
+    column = int(request.args.get('column'))
+    if row != None and column != None:
+        if not(update_state(row,column)):
+            error = "Failed to update game state"         
+            flash(error)
+    return redirect(url_for('.running'))
+
+
+@bp.route('/running_state/<int:row>_<int:column>', methods=['GET'])
 def running_state(row=None, column=None):
-    if request.method == 'GET':
-        game_state = model.get_state()
-        if row==None or column==None:
-            return jsonify(game_state)
-        else:
-            state = game_state[row][column]
-            return jsonify(state)
+    game_state = get_state()
+    if row=="-1" or column=="-1":
+        return jsonify(game_state)
     else:
-        return jsonify(model.update_state(row,column))
+        state = game_state[row][column]
+        return jsonify(state)
 
 
 @bp.route('/start', methods=['GET', 'POST'])
@@ -41,7 +51,7 @@ def start():
             error = "Please specify a positive dimension"
 
         if error is None:
-            if model.create_state(rows, columns):
+            if create_state(rows, columns):
                 return redirect(url_for('game.running'))
             else:
                 error = "Failed to start game"
--- flaskr/model.py
+++ flaskr/model.py
@@ -1,9 +1,10 @@
 max_chain = 1
+state = None
 
 def create_state(r, c):
     try:
         global state 
-        state = [[0] * r for i in range(c)]
+        state = [[1] * c for i in range(r)]
         return True
     except:
         return False
@@ -16,19 +17,24 @@ def update_state(r, c, chain=0):
             state[r][c] = 1
         else:
             state[r][c] = 0
-
         if chain != max_chain:
-            update_state(r+1, c, 1)
-            update_state(r-1, c, 1)
-            update_state(r, c+1, 1)
-            update_state(r, c-1, 1)
-        
+            if r != 0:
+                update_state(r-1,c,1)
+            if r != len(state):
+                update_state(r+1,c,1)
+            if c != 0:
+                update_state(r,c-1,1)
+            if c != len(state[0]):
+                update_state(r,c+1,1)
         return True
     except:
         return False
 
 def get_state():
-    return state
+    if state == None:
+        return False
+    else:
+        return state
 
 
 
